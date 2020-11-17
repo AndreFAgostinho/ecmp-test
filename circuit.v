6086386586c65f210690734491b7f1e4bed33e53
André Agostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2020-11-17 22:27:40 +0000

    Implemented test exercise III

--- circuit.v
+++ circuit.v
@@ -3,15 +3,28 @@
 
 module circuit
   (
-   `INPUT(rst,1),
-   `INPUT(clk,1),
-   `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
-   );
+  `INPUT(rst,1),
+  `INPUT(clk,1),
+  `INPUT(en,1),
+  `INPUT(x,12),
+  `OUTPUT(y,32)
+  );
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
-   
+  `SIGNAL(acc, 32)
+  `SIGNAL(x_abs, 12)
+
+  `COMB begin
+    if (x[11] == 1'b1) begin
+      x_abs = -x;
+    end
+    else begin
+      x_abs = x;
+    end
+  end
+
+
+
+  `ACC_ARE(clk, rst, 1'b0, en, acc, x_abs)
+  `SIGNAL2OUT(y, acc)
+
 endmodule
--- circuit_tb.v
+++ circuit_tb.v
@@ -1,28 +1,32 @@
 `timescale 1ns / 1ps
 `include "iob_lib.vh"
 
+// x is random numbers in the range [-2048,2047] (12 bits)
+// passed through y(n) = y(n-1) + |x(n)| should yield in average y = 1024*n
+
 module circuit_tb;
 
    `CLOCK(clk, 10)
    `RESET(rst, 1, 10)
    `SIGNAL(en, 1)
-   `SIGNAL(x, 32)
+   `SIGNAL_SIGNED(x, 12)
+   `SIGNAL_SIGNED(y_sgn, 32)
    `SIGNAL_OUT(y, 32)
 
    integer i;
-   
+
    initial begin
       $dumpfile("circuit.vcd");
       $dumpvars();
       en=0;
-      for (i=0; i<100; i=i+1) begin
-         @(posedge clk) #1 x=i; en=1;
+      for (i=0; i<101; i=i+1) begin
+         @(posedge clk) #1 x= $random % 2048; y_sgn = y;$display("n = %d |x: %d |y = %d",i, x, y_sgn); en=1;
          @(posedge clk) #1 en=0;
       end
 
       @(posedge clk) #100 $finish;
 
-   end 
+   end
 
    circuit c0
      (
